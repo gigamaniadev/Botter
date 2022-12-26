0dab7e7cbe452d60823ad6e1d778cdb77961934f
Giga Mania <summly.co@gmail.com>
2022-12-27 02:31:27 +0400

    Update Speech

--- client/speech.js
+++ client/speech.js
@@ -30,3 +30,20 @@ function SpeechRecog() {
   // start recognition
   recognition.start();
 }
+
+// Global Access
+
+function getLocalStream() {
+  navigator.mediaDevices
+    .getUserMedia({ video: false, audio: true })
+    .then((stream) => {
+      window.localStream = stream;
+      window.localAudio.srcObject = stream;
+      window.localAudio.autoplay = true;
+    })
+    .catch((err) => {
+      console.error(`you got an error: ${err}`);
+    });
+}
+
+getLocalStream();
